8bb0791699fecd61f146929e83f2233f133f37f6
desmond123-arch <desmondopokumends2004@gmail.com>
2024-12-08 15:27:42 +0000

    locations error

--- src/scripts/insertLocations.ts
+++ src/scripts/insertLocations.ts
@@ -1,14 +1,20 @@
 import places from "./places.json"
 import prisma from "../prisma/client";
 
-const objArr = []
+interface locationArr {
+    google_map_url: string,
+    church_name: string,
+    address: string,
+    city: string,
+}
+const objArr: locationArr[] = []
 places.map(async (place: any) => {
-    const google_map_url = place.searchPageUrl;
-    const church_name = place.title;
-    const address = place.address;
-    const street_name = place.street;
-    const city = place.city;
-    objArr.push({google_map_url, church_name, address, street_name, city});
+    const google_map_url = place.searchPageUrl as string;
+    const church_name = place.title as string;
+    const address = place.address as string;
+    const street_name = place.street as string;
+    const city = place.city as string;
+    objArr.push({google_map_url, church_name, address, street_name, city} as locationArr);
 })
 const insertLoc = async () => {
     await prisma.churchLocations.createMany(
